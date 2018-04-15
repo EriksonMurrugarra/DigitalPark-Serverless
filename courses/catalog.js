c9291a514d830004844f9d1c01fcb87c5314f238
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-04-14 21:53:21 -0500

    Connecting catalog to its database

--- courses/catalog.js
+++ courses/catalog.js
@@ -1,81 +1,33 @@
 'use strict'
+const AWS = require('aws-sdk');
 
-const CATALOG = [
-  {
-    "id": 1,
-    "title": "Docker & Kubernetes",
-    "description": "Contenedores",
-    "courses": [
-      {
-        "id": 11,
-        "title": "Introduccion a Docker",
-        "tags": "DOCKER",
-        "image": "./images/covers/docker.png"
-      },
-      {
-        "id": 12,
-        "title": "Desarrollando con Docker",
-        "tags": "DOCKER",
-        "image": "./images/covers/docker.png"
-      },
-      {
-        "id": 13,
-        "title": "Deployando a Kubernetes",
-        "tags": "DOCKER - KUBERNETES",
-        "image": "./images/covers/docker.png"
-      },
-      {
-        "id": 14,
-        "title": "Usando Docker Compose",
-        "tags": "DOCKER - COMPOSE",
-        "image": "./images/covers/docker.png"
-      }
-    ]
-  },
-  {
-    "id": 2,
-    "title": "DevOps",
-    "description": "DevOps en la practica",
-    "courses": [
-      {
-        "id": 21,
-        "title": "Introduccion a Jenkins",
-        "tags": "DEVOPS - JENKINS",
-        "image": "./images/covers/docker.png"
-      },
-      {
-        "id": 22,
-        "title": "Delivery Pipelines",
-        "tags": "DEVOPS - JENKINS",
-        "image": "./images/covers/docker.png"
-      },
-      {
-        "id": 23,
-        "title": "Pipeline as Code con Jenkins",
-        "tags": "DEVOPS - JENKINS",
-        "image": "./images/covers/docker.png"
-      },
-      {
-        "id": 24,
-        "title": "Integracion Continua",
-        "tags": "DEVOPS - JENKINS",
-        "image": "./images/covers/docker.png"
-      }
-    ]
-  }];
-
+const tableName = `digitalpark-${process.env.SLS_STAGE}-courses-catalog`;
+const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
 
 module.exports.handler = (event, context, callback) => {
-  console.log(JSON.stringify(event));
-  callback(null,  
+  const query = {
+    TableName: tableName
+  };
+
+  ddb.scan(query, (err, data) => {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+
+    let catalog = data.Items.map(
+      record => AWS.DynamoDB.Converter.unmarshall(record)
+    );
+
+    callback(null,  
       {
         "statusCode": 200,
-        "body": JSON.stringify(CATALOG),
+        "body": JSON.stringify(catalog),
         "headers": {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*"
         }
       }
-    ); 
-
+    );
+  }); 
 }
